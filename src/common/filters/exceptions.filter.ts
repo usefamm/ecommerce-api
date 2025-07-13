8a7c6474f79b11829d9275744aa276775839a5b8
usef <karimiusef679@gmail.com>
2025-07-14 00:51:43 +0330

    feat: add exception handler

---
+++ src/common/filters/exceptions.filter.ts
@@ -0,0 +1,35 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  Logger,
+} from '@nestjs/common';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  private logger = new Logger('Exceptions');
+
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    const status =
+      exception instanceof HttpException ? exception.getStatus() : 500;
+
+    const message =
+      exception instanceof HttpException ? exception.getResponse() : exception;
+
+    this.logger.error(
+      `❌ ${request.method} ${request.url} -> Status ${status} - Error: ${JSON.stringify(message)}`,
+    );
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      error: message,
+    });
+  }
+}
